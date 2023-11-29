3124881df58ddc0cd52d1e379d4d61d7c309cbf2
Arviansyah Eka <arviansayhekapk@gmail.com>
2023-11-29 09:27:37 +0700

    commit 2

--- app/src/main/java/com/example/prakmobileuas/ui/LoginActivity.kt
+++ app/src/main/java/com/example/prakmobileuas/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.prakmobileuas.ui
 
+// LoginActivity.kt
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -8,9 +9,10 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.prakmobileuas.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.AuthResult
-import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class LoginActivity : AppCompatActivity() {
 
@@ -25,49 +27,68 @@ class LoginActivity : AppCompatActivity() {
         val etUsername = findViewById<EditText>(R.id.username)
         val etPassword = findViewById<EditText>(R.id.password)
         val btnLogin = findViewById<Button>(R.id.login)
+        val btnRegister = findViewById<Button>(R.id.daftar)
 
         btnLogin.setOnClickListener {
             val username = etUsername.text.toString().trim()
             val password = etPassword.text.toString().trim()
 
-            loginWithFirebase(username, password)
+            loginUser(username, password)
         }
-    }
 
-    private fun loginWithFirebase(username: String, password: String) {
-        mAuth.signInWithEmailAndPassword(username, password)
-            .addOnCompleteListener(this, OnCompleteListener<AuthResult> { task ->
-                if (task.isSuccessful) {
-                    // Login berhasil, arahkan ke aktivitas selanjutnya
-                    navigateToNextActivity()
-                } else {
-                    // Jika login gagal, tampilkan pesan kesalahan
-                    Toast.makeText(this@LoginActivity, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                }
-            })
-    }
-
-    private fun navigateToNextActivity() {
-        // Tentukan peran pengguna (Admin atau User) dan arahkan ke aktivitas yang sesuai
-        val currentUser: FirebaseUser? = mAuth.currentUser
-        if (currentUser != null) {
-            val username = currentUser.email ?: "" // Jika email null, set sebagai string kosong
-            val intent = if (isAdmin(username)) {
-                Intent(this@LoginActivity, AdminActivity::class.java)
-            } else {
-                Intent(this@LoginActivity, UserActivity::class.java)
-            }
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
             finish()
         }
     }
 
+    private fun loginUser(username: String, password: String) {
+        mAuth.signInWithEmailAndPassword(username, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = mAuth.currentUser
+                    getUserRoleFromDatabase(user?.uid) { role ->
+                        if (role == "admin") {
+                            // Pengguna adalah admin, lakukan sesuatu
+                            Toast.makeText(this@LoginActivity, "Login berhasil sebagai Admin!", Toast.LENGTH_SHORT).show()
+                        } else {
+                            // Pengguna adalah pengguna biasa, lakukan sesuatu yang berbeda
+                            Toast.makeText(this@LoginActivity, "Login berhasil sebagai Pengguna!", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this@LoginActivity, "Login gagal. Silakan coba lagi.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 
-    private fun isAdmin(username: String): Boolean {
-        // Implementasikan logika untuk menentukan apakah pengguna adalah admin
-        // Misalnya, jika email pengguna mengandung "admin", return true
-        return username.contains("admin", ignoreCase = true)
+    private fun getUserRoleFromDatabase(userId: String?, callback: (String?) -> Unit) {
+        val databaseReference = FirebaseDatabase.getInstance().getReference("users")
+        val userReference = databaseReference.child(userId ?: "")
+        val roleReference = userReference.child("role")
+
+        roleReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val role = dataSnapshot.getValue(String::class.java)
+                callback.invoke(role)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle errors if needed
+                callback.invoke(null)
+            }
+        })
+    }
+
+    private fun handleUserRole(role: String?) {
+        if (role == "admin") {
+            // User is an admin, perform admin actions
+            Toast.makeText(this@LoginActivity, "Login berhasil sebagai Admin!", Toast.LENGTH_SHORT).show()
+        } else {
+            // User is a regular user, perform user actions
+            Toast.makeText(this@LoginActivity, "Login berhasil sebagai Pengguna!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
 
---
+++ app/src/main/java/com/example/prakmobileuas/ui/RegisterActivity.kt
@@ -0,0 +1,76 @@
+package com.example.prakmobileuas.ui
+// RegisterActivity.kt
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.prakmobileuas.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.database.FirebaseDatabase
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val etUsername = findViewById<EditText>(R.id.username)
+        val etPassword = findViewById<EditText>(R.id.password)
+        val btnRegister = findViewById<Button>(R.id.register)
+        val btnLogin = findViewById<Button>(R.id.login)
+
+        btnRegister.setOnClickListener {
+            val username = etUsername.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            registerUser(username, password)
+        }
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun registerUser(username: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(username, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = mAuth.currentUser
+                    val profileUpdates = UserProfileChangeRequest.Builder()
+                        .setDisplayName(username)
+                        .build()
+                    user?.updateProfile(profileUpdates)
+
+                    // Menambahkan informasi peran ke database Firebase Realtime Database
+                    val role = "user" // Default role, dapat diubah menjadi "admin" jika diperlukan
+                    saveUserRole(user?.uid, role)
+
+                    Toast.makeText(this@RegisterActivity, "Registrasi berhasil!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@RegisterActivity, UserActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    if (task.exception is FirebaseAuthUserCollisionException) {
+                        Toast.makeText(this@RegisterActivity, "Email sudah digunakan.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this@RegisterActivity, "Registrasi gagal. Silakan coba lagi.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+    }
+
+    private fun saveUserRole(userId: String?, role: String) {
+        val databaseReference = FirebaseDatabase.getInstance().getReference("users")
+        databaseReference.child(userId ?: "").child("role").setValue(role)
+    }
+}
